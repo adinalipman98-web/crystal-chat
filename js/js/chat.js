15503fd9ceea0fb87ee4635f55e7431d01f32f6f
adinalipman98-web <adinalipman98@gmail.com>
2026-07-29 08:11:41 +0300

    Update chat.js

--- js/js/chat.js
+++ js/js/chat.js
@@ -23,3 +23,29 @@ sendButton.addEventListener("click", async () => {
     messageInput.value = "";
 
 });
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
+const q = query(
+    collection(db, "messages"),
+    orderBy("createdAt")
+);
+
+onSnapshot(q, (snapshot) => {
+
+    messages.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+
+        const data = doc.data();
+
+        const message = document.createElement("p");
+
+        message.innerHTML = `
+        <b>${data.sender}:</b> ${data.text}
+        `;
+
+        messages.appendChild(message);
+
+    });
+
+});
