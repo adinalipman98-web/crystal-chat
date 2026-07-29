cea547b625790d281ea5115ceed604d951868e25
adinalipman98-web <adinalipman98@gmail.com>
2026-07-29 08:09:52 +0300

    Update chat.js

--- js/js/chat.js
+++ js/js/chat.js
@@ -1,1 +1,25 @@
+import { db } from "./firebase.js";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
+const sendButton = document.getElementById("sendButton");
+const messageInput = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
+
+sendButton.addEventListener("click", async () => {
+
+    const text = messageInput.value;
+
+    if (text.trim() === "") {
+        return;
+    }
+
+    await addDoc(collection(db, "messages"), {
+        text: text,
+        sender: localStorage.getItem("username") || "אורח",
+        room: "General",
+        createdAt: serverTimestamp()
+    });
+
+    messageInput.value = "";
+
+});
